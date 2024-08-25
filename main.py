47d149fd5c19539b4d693af6cd00a7f1606899f3
jericho909 <omerahmetyucel@gmail.com>
2024-08-25 18:33:53 +0300

    edits

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(char_count(text))
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def word_counter(text):
+    word_list = text.split()
+    return len(word_list)
+
+def char_count(text):
+    lower_text = text.lower()
+    char_dict = {
+
+    }
+    for char in lower_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+          char_dict[char] = 1  
+
+    return char_dict
+
+main()
