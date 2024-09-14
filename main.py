b99b4a7e0198f582560cf128ba4debb34f4bf51e
jericho909 <omerahmetyucel@gmail.com>
2024-09-14 15:14:29 +0300

    finished

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(char_count(text))
+    word_count = word_counter(text)
+    print(f"{word_count} words found in document")
+    list_of_dicts = dict_to_list(char_count(text))
+    sorted_list_of_dicts = sorted(list_of_dicts, key=lambda d: list(d.values())[0], reverse= True)
+
+
+    for k in sorted_list_of_dicts:
+        for key,value in k.items():
+            print(f"The {key} character was found {value} times.")
 
 
 def get_book_text(path):
@@ -19,10 +27,16 @@ def char_count(text):
     }
     for char in lower_text:
         if char in char_dict:
-            char_dict[char] += 1
+            if char.isalpha():
+                char_dict[char] += 1
         else:
           char_dict[char] = 1  
 
     return char_dict
+
+def dict_to_list(dict):
+    list_of_dicts = [{key: value} for key, value in dict.items()]
+    return list_of_dicts
+
 
 main()
